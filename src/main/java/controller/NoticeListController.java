f8558b74f4727c65999333cbc8ddbdb775a1e86d
Dong1333 <mogijisung13@gmail.com>
2022-12-06 18:26:42 +0900

    목록에서 페이징 구현하기
    아직 구현해야 할 것들
    1. 페이지 번호 하이라이트 구분(선택 번호만 색상)
    2. 현제 페이지 위치 표시(우측 하단)
    3. 데이터가 있지 않은(레코드가 없는 페이지)페이지 번호 미노출

--- src/main/java/controller/NoticeListController.java
+++ src/main/java/controller/NoticeListController.java
@@ -22,19 +22,24 @@ public class NoticeListController extends HttpServlet {
 		// 우선 검색어 전달이 안 됐을때 대비하여 임시 변수선언 하기 (값이 들어오면 지금 선언한 변수값이 할당)
 		String field_ = request.getParameter("f"); // 필드 기준으로으어떤 기준으로 전달할 지
 		String query_ = request.getParameter("q"); // 사용자가 입력한 검색어(검색 값)
-
+		String page_ = request.getParameter("p");
+		
 		// 실제로 전달이 안됐을 때 (null 이면) "title" 기본 값 지정
 		// 값이 전달 돼으면 field_으로 전달 받은 값 field에 넣어주기
 		String field = "title";
-		if(field_ != null)
+		if(field_ != null && !field_.equals(""))
 			field = field_;
 		
 		String query = "";
-		if(query_ != null)
+		if(query_ != null && !query_.equals(""))
 			query = query_;
 		
+		int page = 1;
+		if(page_ != null && !page_.equals(""))
+			page = Integer.parseInt(page_);
+		
 		NoticeService service = new NoticeService();
-		List<Notice> list = service.getNoticeList(field, query, 1);
+		List<Notice> list = service.getNoticeList(field, query, page);
 		
 		request.setAttribute("list", list);
 		
