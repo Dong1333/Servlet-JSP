503f72713166ba80ab3c09db537680632b2b2e58
Dong1333 <mogijisung13@gmail.com>
2022-12-07 17:50:55 +0900

    81 - 자세한 페이지 수정하기

--- src/main/java/controller/NoticeDetailController.java
+++ src/main/java/controller/NoticeDetailController.java
@@ -1,12 +1,6 @@
  package controller;
 
 import java.io.IOException;
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.util.Date;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -15,73 +9,23 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import entity.Notice;
+import service.NoticeService;
 
 @WebServlet("/notice/detail")
 public class NoticeDetailController extends HttpServlet{
 	@Override
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-
+			// 사용자가 요청할때 전달 했던 ID값
 			int id = Integer.parseInt(request.getParameter("id"));
-			String sUrl = "jdbc:mysql://localhost:3306/firstDB";
-			String sUser = "root";
-			String sPwd = "12341234";
 			
-			String sSql = "SELECT * FROM NOTICE WHERE ID=?;"; 
+			NoticeService service = new NoticeService();
+			Notice notice = service.getNotice(id); // 서비스 Notice 하나를 얻는 메소드
 			
-			try {
-				Class.forName("com.mysql.jdbc.Driver");
-				Connection MyConn = DriverManager.getConnection(sUrl, sUser, sPwd);
-				
-				PreparedStatement st = MyConn.prepareStatement(sSql);
-				st.setInt(1, id);
-				ResultSet rs = st.executeQuery();
-				
-
-				// model 변수 생성
-				rs.next();
-				String title = rs.getString("TITLE"); 
-				String writeid =rs.getString("WRITER_ID"); 
-				Date regdate =rs.getDate("REGDATE");
-				String hit =rs.getString("HIT"); 
-				String files =rs.getString("FILES");	
-				String content =rs.getString("CONTENT"); 
-				
-				request.setAttribute("title", title);
-				request.setAttribute("writeid", writeid);
-				request.setAttribute("regdate", regdate);
-				request.setAttribute("hit", hit);
-				request.setAttribute("files", files);
-				request.setAttribute("content", content);
-				
-				Notice notice = new Notice(
-							id, 
-							title,
-							writeid,
-							regdate,
-							hit,
-							files,
-							content
-						);
-				
-				request.setAttribute("n", notice);
-				
-				
-					
-			    
-			    
-			 	rs.close();
-			 	st.close();
-			 	MyConn.close();
-			  
-			} catch (ClassNotFoundException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			} catch (SQLException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
+			request.setAttribute("n", notice); // 받은 notice를 "n"이라는 키워드로 전달
 			
 			
+			// forward
+			// 사용자에게 돌려주는 출력 로직
 			request
 			.getRequestDispatcher("/WEB-INF/view/notice/detail.jsp")
 			.forward(request, response);
